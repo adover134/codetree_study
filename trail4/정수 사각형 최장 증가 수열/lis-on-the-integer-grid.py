087109e816501fad685fce9b40314f5c771d355a
adover134 <45279353+adover134@users.noreply.github.com>
2026-09-12 23:26:44 +0900

    [Passed] 정수 사각형 최장 증가 수열 (592ms, 52MB)

--- trail4/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ trail4/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -2,11 +2,10 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-# 주위 4칸이 모두 자신보다 작다면
-# 모두 시작 칸으로 지정한다.
 nums = sorted((grid[i][j], i, j) for i in range(n) for j in range(n))
 dx, dy = [1,-1,0,0],[0,0,1,-1]
 Map = [[0 for _ in range(n)] for _ in range(n)]
+maxi = 0
 for v, x, y in nums:
     m = 0
     for d in range(4):
@@ -16,4 +15,5 @@ for v, x, y in nums:
         if grid[ax][ay]<grid[x][y]:
             m = max(Map[ax][ay], m)
     Map[x][y] = m+1
-print(max(max(m) for m in Map))
+    maxi = max(Map[x][y], maxi)
+print(maxi)
