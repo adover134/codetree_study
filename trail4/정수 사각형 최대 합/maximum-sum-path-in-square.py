852bcc9fd1dfb274f6ec6df852fb67cad62c1f4f
adover134 <45279353+adover134@users.noreply.github.com>
2026-09-12 22:36:38 +0900

    [Passed] 정수 사각형 최대 합 (51ms, 16MB)

---
+++ trail4/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,12 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+for i in range(1, n):
+    grid[i][0] += grid[i-1][0]
+    grid[0][i] += grid[0][i-1]
+for i in range(1, n):
+    for j in range(1, n):
+        grid[i][j] += max(grid[i-1][j], grid[i][j-1])
+print(grid[n-1][n-1])
