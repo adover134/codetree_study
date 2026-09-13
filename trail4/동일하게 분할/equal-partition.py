e445e1a3df8ea7fd5b28e1b23a141ac3f67196fa
adover134 <45279353+adover134@users.noreply.github.com>
2026-09-13 19:24:25 +0900

    [Passed] 동일하게 분할 (296ms, 55MB)

--- trail4/동일하게 분할/equal-partition.py
+++ trail4/동일하게 분할/equal-partition.py
@@ -6,22 +6,23 @@ arr = list(map(int, input().split()))
 # 차이가 j인 경우가 있는가?
 
 # offset은 100000이면 되겠어.
-import sys
-S = sum(arr)
-if S % 2 == 1:
+offset=100000
+S=sum(arr)
+if S%2 == 1:
     print("No")
+    exit(0)
+dp=[[False for i in range(S//2+1)] for _ in range(len(arr)+1)]
+dp[0][0]=True
+for i in range(1,len(arr)+1):
+    for j in range(S//2+1):
+        t=j-arr[i-1]
+        if dp[i-1][j]:
+            dp[i][j]=True
+        if t<0:
+            continue
+        elif dp[i-1][t]:
+            dp[i][j]=True
+if dp[-1][S//2]:
+    print("Yes")
 else:
-    T = S//2
-    dp = [False]*(T+1)
-    arr=sorted(arr,reverse=True)
-    for num in arr:
-        if num > T:
-            break
-        for t in range(T,num,-1):
-            if dp[t-num]:
-                dp[t]=True
-        dp[num]=True
-    if dp[T]:
-        print("Yes")
-    else:
-        print("No")
+    print("No")
