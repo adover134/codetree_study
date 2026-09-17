626a2f5dd356d78a5b7762a398455b4a9e13da4c
adover134 <45279353+adover134@users.noreply.github.com>
2026-09-17 21:37:02 +0900

    [Passed] 숫자 퍼즐 (91ms, 20MB)

--- trail4/숫자 퍼즐/number-puzzle.py
+++ trail4/숫자 퍼즐/number-puzzle.py
@@ -9,59 +9,38 @@ n, m, k = map(int, input().split())
 
 dp = [[[-float('inf') for _ in range(m+1)] for _ in range(m+1)] for _ in range(n + 1)]
 
-# pos: 이번 위치
-# mini: 직전 위치에서 선택한 최솟값
-# M: 남은 합
-# dp[pos][mini][M]은 pos번째 숫자로 mini를 골랐고, pos를 포함해서 이후까지 합이 mini여야 한다.
-
-def fill_dp(pos, prev, M):
-    # pos 위치에 prev를 넣은 상태에서 나머지를 채울 수 있는 가짓수를 구하는 것이 목표이다.
-    # 값은 prev 이상이어야 한다.
-    # 남은 수는 M-prev이다.
-    remaining = M-prev
-
-    if pos == (n-1):
+def fill_dp(coins, prev, remaining):
+    if (prev * coins) > remaining:
+        return 0
+    global dp
+    s = 0
+    if coins == 0:
         if remaining == 0:
+            dp[coins][prev][remaining] = 1
             return 1
         else:
+            dp[coins][prev][remaining] = 0
             return 0
-    
-    s = 0
-    # pos + 1 위치에는
-    # prev 이상의 수만 들어갈 수 있다.
+    if dp[coins][prev][remaining]==-float('inf'):
+        dp[coins][prev][remaining] = 0
+        if coins >= 1:
+            dp[coins][prev][remaining] = fill_dp(coins-1, prev, remaining-prev)
+        if (prev+1) <= remaining:
+            dp[coins][prev][remaining] += fill_dp(coins, prev+1, remaining)
+    return dp[coins][prev][remaining]
+fill_dp(n, 1, m)
 
-    # pos + 1 위치에 i를 넣을 때
-    for i in range(prev, remaining+1):
-        global dp
-        # 다음 위치는 pos+1
-        # 다음 최소값은 i
-        # 다음 M은 remaining
-        if dp[pos+1][i][remaining] == -float('inf'):
-            dp[pos+1][i][remaining] = fill_dp(pos+1, i, remaining)
-        s += dp[pos+1][i][remaining]
-    return s
-for i in range(m+1):
-    dp[0][i][m] = fill_dp(0, i, m)
+# dp[coins][value][remaining] = dp[coins-1][value][remaining-value] + dp[coins][value+1][remaining]
 
+coins, value, remaining = n, 1, m
 s = 0
-i = 0
-j = 1
-M = m
-
-ans = []
-
-while s < k:
-    if s + dp[i][j][M] >= k:
-        ans.append(str(j))
-        if i < (n-1):
-            i += 1
-            M -= j
-        else:
-            break
+while coins > 0:
+    cnt = dp[coins-1][value][remaining-value]
+    if s+ cnt < k:
+        if cnt > 0:
+            s += cnt
+        value += 1
     else:
-        if dp[i][j][M] > 0:
-            s += dp[i][j][M]
-        j += 1
-    if i == n or j > M or M < 0:
-        break
-print(' '.join(ans))
+        coins -= 1
+        remaining -= value
+        print(value, end = ' ')
