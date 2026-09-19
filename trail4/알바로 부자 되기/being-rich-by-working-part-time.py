55a014b356f6ef3229c6fd8fcfa16a15e26e819f
adover134 <45279353+adover134@users.noreply.github.com>
2026-09-19 18:36:13 +0900

    [Passed] 알바로 부자 되기 (54ms, 16MB)

--- trail4/알바로 부자 되기/being-rich-by-working-part-time.py
+++ trail4/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -1,15 +1,25 @@
 n = int(input())
-jobs = sorted(tuple(map(int, input().split())) for _ in range(n))
+jobs = [tuple(map(int, input().split())) for _ in range(n)]
 s = [job[0] for job in jobs]
 e = [job[1] for job in jobs]
 p = [job[2] for job in jobs]
 
 # Please write your code here.
-dp = [0 for _ in range(n)]
+# dp는 n개 두고
+# 종료 시점 기준으로 정렬된 dp를 활용한다.
+e = sorted(list(enumerate(e)), key = lambda x: x[1])
+dp = [0 for _ in range(n+1)]
 # dp[i]는 i번째 알바를 할 때 얻을 수 있는 최댓값이다.
+ends = -1
+ptr = 0
 for i in range(n):
-    dp[i] = p[i]
-    for j in range(i):
-        if s[i] > e[j]:
-            dp[i] = max(dp[i], dp[j] + p[i])
+    while e[ptr][1] < s[i]:
+        j = e[ptr][0]
+        if ends == -1 or dp[j] > dp[ends]:
+            ends = j
+        ptr += 1
+    if ends == -1:
+        dp[i] = p[i]
+    else:
+        dp[i] = max(dp[i], dp[ends]+p[i])
 print(max(dp))
